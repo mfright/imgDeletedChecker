abfb8c9b8b32df1835a5a7d9b6e7841e2595936c
mfright <uj020288@yahoo.co.jp>
2015-06-06 17:29:08 +0900

    r1
    r1 released

---
+++ imageDeletedChecker/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace imageDeletedChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelectNewHtml = new System.Windows.Forms.Button();
+            this.lblHtmlFile = new System.Windows.Forms.Label();
+            this.lstImagesNew = new System.Windows.Forms.ListBox();
+            this.lstImagesPrevious = new System.Windows.Forms.ListBox();
+            this.lblPreviousFile = new System.Windows.Forms.Label();
+            this.btnSelectPreviousHtml = new System.Windows.Forms.Button();
+            this.lstImagesDeleted = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.webPreview = new System.Windows.Forms.WebBrowser();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectNewHtml
+            // 
+            this.btnSelectNewHtml.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSelectNewHtml.Location = new System.Drawing.Point(576, 7);
+            this.btnSelectNewHtml.Name = "btnSelectNewHtml";
+            this.btnSelectNewHtml.Size = new System.Drawing.Size(176, 25);
+            this.btnSelectNewHtml.TabIndex = 0;
+            this.btnSelectNewHtml.Text = "1. Select new html";
+            this.btnSelectNewHtml.UseVisualStyleBackColor = false;
+            this.btnSelectNewHtml.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblHtmlFile
+            // 
+            this.lblHtmlFile.AutoSize = true;
+            this.lblHtmlFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblHtmlFile.Location = new System.Drawing.Point(11, 7);
+            this.lblHtmlFile.Name = "lblHtmlFile";
+            this.lblHtmlFile.Size = new System.Drawing.Size(310, 12);
+            this.lblHtmlFile.TabIndex = 1;
+            this.lblHtmlFile.Text = "最新のHTMLファイルを選択。IMGタグは <img src=\" を含むこと。";
+            // 
+            // lstImagesNew
+            // 
+            this.lstImagesNew.FormattingEnabled = true;
+            this.lstImagesNew.ItemHeight = 12;
+            this.lstImagesNew.Location = new System.Drawing.Point(16, 86);
+            this.lstImagesNew.Name = "lstImagesNew";
+            this.lstImagesNew.Size = new System.Drawing.Size(287, 280);
+            this.lstImagesNew.TabIndex = 2;
+            // 
+            // lstImagesPrevious
+            // 
+            this.lstImagesPrevious.FormattingEnabled = true;
+            this.lstImagesPrevious.ItemHeight = 12;
+            this.lstImagesPrevious.Location = new System.Drawing.Point(309, 86);
+            this.lstImagesPrevious.Name = "lstImagesPrevious";
+            this.lstImagesPrevious.Size = new System.Drawing.Size(300, 184);
+            this.lstImagesPrevious.TabIndex = 3;
+            // 
+            // lblPreviousFile
+            // 
+            this.lblPreviousFile.AutoSize = true;
+            this.lblPreviousFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblPreviousFile.Location = new System.Drawing.Point(11, 33);
+            this.lblPreviousFile.Name = "lblPreviousFile";
+            this.lblPreviousFile.Size = new System.Drawing.Size(314, 12);
+            this.lblPreviousFile.TabIndex = 4;
+            this.lblPreviousFile.Text = "以前のHTMLファイルを選択。IMGタグは <img src=\"　を含むこと。";
+            // 
+            // btnSelectPreviousHtml
+            // 
+            this.btnSelectPreviousHtml.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnSelectPreviousHtml.Location = new System.Drawing.Point(576, 38);
+            this.btnSelectPreviousHtml.Name = "btnSelectPreviousHtml";
+            this.btnSelectPreviousHtml.Size = new System.Drawing.Size(176, 23);
+            this.btnSelectPreviousHtml.TabIndex = 5;
+            this.btnSelectPreviousHtml.Text = "2. Select previous html";
+            this.btnSelectPreviousHtml.UseVisualStyleBackColor = false;
+            this.btnSelectPreviousHtml.Click += new System.EventHandler(this.btnSelectPreviousHtml_Click);
+            // 
+            // lstImagesDeleted
+            // 
+            this.lstImagesDeleted.FormattingEnabled = true;
+            this.lstImagesDeleted.ItemHeight = 12;
+            this.lstImagesDeleted.Location = new System.Drawing.Point(615, 86);
+            this.lstImagesDeleted.Name = "lstImagesDeleted";
+            this.lstImagesDeleted.Size = new System.Drawing.Size(303, 184);
+            this.lstImagesDeleted.TabIndex = 6;
+            this.lstImagesDeleted.SelectedIndexChanged += new System.EventHandler(this.lstImagesDeleted_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "最新htmlの img src=\" の参照先";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "以前のhtmlの img src=\"　の参照先";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(613, 71);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(196, 12);
+            this.Label4.TabIndex = 9;
+            this.Label4.Text = "以前のhtmlにのみ含まれるimgの参照先";
+            // 
+            // webPreview
+            // 
+            this.webPreview.Location = new System.Drawing.Point(309, 276);
+            this.webPreview.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webPreview.Name = "webPreview";
+            this.webPreview.Size = new System.Drawing.Size(598, 297);
+            this.webPreview.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "プレビュー";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCompare.Location = new System.Drawing.Point(758, 7);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(149, 54);
+            this.btnCompare.TabIndex = 12;
+            this.btnCompare.Text = "3. COMPARE";
+            this.btnCompare.UseVisualStyleBackColor = false;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 585);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.webPreview);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstImagesDeleted);
+            this.Controls.Add(this.btnSelectPreviousHtml);
+            this.Controls.Add(this.lblPreviousFile);
+            this.Controls.Add(this.lstImagesPrevious);
+            this.Controls.Add(this.lstImagesNew);
+            this.Controls.Add(this.lblHtmlFile);
+            this.Controls.Add(this.btnSelectNewHtml);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IMG誤削除チェッカー";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectNewHtml;
+        private System.Windows.Forms.Label lblHtmlFile;
+        private System.Windows.Forms.ListBox lstImagesNew;
+        private System.Windows.Forms.ListBox lstImagesPrevious;
+        private System.Windows.Forms.Label lblPreviousFile;
+        private System.Windows.Forms.Button btnSelectPreviousHtml;
+        private System.Windows.Forms.ListBox lstImagesDeleted;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.WebBrowser webPreview;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCompare;
+    }
+}
+
---
+++ imageDeletedChecker/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageDeletedChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private String selectedFile()
+        {
+
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "default.html";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            //ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに
+            //「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                //選択されたファイル名を表示する
+                //Console.WriteLine(ofd.FileName);
+                return ofd.FileName;
+            }
+
+            return "";
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            lblHtmlFile.Text = selectedFile();
+
+            loadUrls(lblHtmlFile.Text, lstImagesNew);
+        }
+
+        private void btnSelectPreviousHtml_Click(object sender, EventArgs e)
+        {
+            lblPreviousFile.Text = selectedFile();
+
+            loadUrls(lblPreviousFile.Text, lstImagesPrevious);
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            lstImagesDeleted.Items.Clear();
+
+            for (int i = 0; i < lstImagesPrevious.Items.Count; i++)
+            {
+                string prevImage = (string)lstImagesPrevious.Items[i];
+
+                for (int j = 0; j < lstImagesNew.Items.Count; j++)
+                {
+                    string newImage = (string)lstImagesNew.Items[j];
+
+                    if (newImage.Equals(prevImage))
+                    {
+                        break;
+                    }
+                    else if (j == lstImagesNew.Items.Count - 1)
+                    {
+                        lstImagesDeleted.Items.Add(prevImage);
+                    }
+                }
+            }
+        }
+
+        private void loadUrls(string filePath,ListBox lstTarget)
+        {
+            //"C:\test\1.txt"をShift-JISコードとして開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                filePath,
+                System.Text.Encoding.GetEncoding("shift_jis"));
+
+
+            //内容を一行ずつ読み込む
+            string line="";
+            string url = "";
+            int startIndex = 0;
+            int endIndex = 0;
+
+            while (sr.Peek() > -1)
+            {
+                line = sr.ReadLine();
+                //Console.WriteLine(line);
+                startIndex = line.IndexOf("img src=");
+                if (startIndex > 0)
+                {
+                    startIndex += 9;
+                    endIndex = line.IndexOf("\"", startIndex);
+                    if (endIndex > 0)
+                    {
+                        url = line.Substring(startIndex, endIndex - startIndex);
+                        lstTarget.Items.Add(url);
+                    }
+                }
+            }
+            //閉じる
+            sr.Close();
+        }
+
+        private void lstImagesDeleted_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedI = lstImagesDeleted.SelectedIndex;
+            string url = (string)lstImagesDeleted.Items[selectedI];
+            webPreview.Navigate(url);
+        }
+
+        /*
+        private string readText(String filePath)
+        {
+            //"C:\test\1.txt"をShift-JISコードとして開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                filePath,
+                System.Text.Encoding.GetEncoding("shift_jis"));
+            //内容をすべて読み込む
+            string s = sr.ReadToEnd();
+            //閉じる
+            sr.Close();
+
+            //結果を出力する
+            return s;
+        }
+
+        private void cropSrcs(String code,ListBox lstTarget)
+        {
+            //var cursor = 0;     // カーソル。順次後ろへ
+            var startIndex = 0; // img srcのURLの開始番地
+            var endIndex = 0;   // img srcのURLの終了番地
+            var url = "";
+
+            for (int i = 0; i < 65530; i++)
+            {
+                startIndex = code.IndexOf("img src=",endIndex)+9;
+                Console.WriteLine("startIndex:"+startIndex);
+
+                if (startIndex == -1)
+                {
+                    i = 65534;
+                    break;
+                }
+                
+                endIndex = code.IndexOf("\"", startIndex);
+                if (endIndex == -1)
+                {
+                    i = 65534;
+                    break;
+                }
+
+                Console.WriteLine("endIndex:" + endIndex);
+
+
+                url = code.Substring(startIndex, endIndex - startIndex);
+                
+                //lstTarget.Items.Add(url);
+
+                Console.WriteLine(" " + url);
+
+            }
+        }
+         */
+    }
+}
